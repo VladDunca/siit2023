f0b84d168adb28c7ae5c708c080920e75316cdf8
Vlad Dunca <vladdnk18@gmail.com>
2023-11-20 19:54:29 +0200

    This is the homework for Week 6 (Testing) Changes: -I forgot to add the parameterized test and added one in CalculatorTest

--- VladDuncaHW8/src/test/java/org/example/CalculatorTest.java
+++ VladDuncaHW8/src/test/java/org/example/CalculatorTest.java
@@ -1,14 +1,22 @@
 package org.example;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.example.Calculator;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CalculatorTest {
 
+    @AfterEach
+    void restoreSystemInput() {
+        System.setIn(System.in);
+    }
     @Test
     void testCalculateValidExpression() {
         String input = "12 cm + 20 mm - 3 km";
@@ -27,5 +35,18 @@ class CalculatorTest {
 
         Calculator calculator = new Calculator(scanner);
         assertEquals("Invalid Format", calculator.calculate());
+    }
+    @ParameterizedTest
+    @CsvSource({
+            "12 cm + 20 mm - 3 km, -2999860",
+            "2 m - 5 cm + 3 km, 3001950"
+    })
+    void testCalculateWithDifferentInputs(String inputExpression, String expectedResult) {
+        InputStream inputStream = new ByteArrayInputStream(inputExpression.getBytes());
+        System.setIn(inputStream);
+
+        Calculator calculator = new Calculator();
+        String result = calculator.calculate();
+        assertEquals("= " + expectedResult + " mm", result.trim());
     }
 }
